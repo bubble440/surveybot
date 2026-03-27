8396ef7d1f695e4ec2d605974f453c6bb085b97b
Wilfried <wilsaa456@gmail.com>
2026-03-27 10:25:03 +0100

    Time management.

--- surveybot/launch.py
+++ surveybot/launch.py
@@ -458,7 +458,6 @@ def init_session_and_enter_surveys(driver, config, account_id: str, notify_fn):
     snap(driver, "after_login")
     time.sleep(15 if _PLM else 3)
     go_to_best_value_survey(driver)
-    snap(driver, "after_navigate_best_value")
 
     return api_key, payout_name, payout_revolut_tag
 
